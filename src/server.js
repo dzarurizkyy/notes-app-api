46f8a6e8d588ba366c70c3207a27098f5a4c8ab0
dzarurizkyy <dzarurizkybusiness@gmail.com>
2023-09-20 17:22:35 +0700

    Create HTTP Server with Hapi Framework

--- src/server.js
+++ src/server.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+// Server Side Framework (Hapi)
+const Hapi = require('@hapi/hapi');
+
+// Create Web Server
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: "localhost",    
+  });
+
+  await server.start();
+  console.log(`Server running on ${server.info.uri}`);
+};
+
+// Turn-on Web Server
+init();
